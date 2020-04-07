16ff79a94f8fbe43ba944107c38cffa0ddc20b4c
Isaac Obuya <Szack4477@gmail.com>
2020-04-07 08:00:34 +0300

    add different circular avatar color based on priority

--- lib/screens/todosList.dart
+++ lib/screens/todosList.dart
@@ -70,8 +70,8 @@ class _TodoListState extends State<TodoList> {
           elevation: 2.0,
           child: ListTile(
             leading: CircleAvatar(
-              backgroundColor: Colors.red,
-              child: Text(this.todos[position].id.toString()),
+              backgroundColor: getColor(this.todos[position].priority),
+              child: Text(this.todos[position].priority.toString()),
             ),
             title: Text(this.todos[position].title),
             subtitle: Text(this.todos[position].date),
@@ -81,5 +81,22 @@ class _TodoListState extends State<TodoList> {
           )
         );
       });
+  }
+
+  // displaying different colors based on the priority
+  Color getColor(int priority) {
+    switch(priority) {
+      case 1:
+        return Colors.red;
+      break;
+      case 2:
+        return Colors.orange;
+      break;
+      case 3:
+        return Colors.green;
+      break;
+      default:
+         return Colors.green;
+    }
   }
 }
